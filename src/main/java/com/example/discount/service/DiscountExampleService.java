5036f44aa4ff4c7fb24c28561a45336c8ec1ab6e
pathak001 <54776760+pathak001@users.noreply.github.com>
2019-10-01 01:20:27 +0530

    update file

--- src/main/java/com/example/discount/service/DiscountExampleService.java
+++ src/main/java/com/example/discount/service/DiscountExampleService.java
@@ -1,1 +1,50 @@
+package com.example.discount.service;
 
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.stereotype.Service;
+
+import com.example.discount.model.ItemType;
+import com.example.discount.model.Order;
+import com.example.discount.util.Constants;
+
+@Service
+public class DiscountExampleService {
+
+	public int calculateDiscount(Order order) {
+
+		int totalAmount = order.getItemDetails().stream().mapToInt(a -> a.getAmount()).reduce(0, Integer::sum);
+		int amountWithoutGrocery = order.getItemDetails().stream()
+				.filter(i -> !(i.getItemType().equals(ItemType.GROCERY))).mapToInt(a -> a.getAmount())
+				.reduce(0, Integer::sum);
+		
+		int percentDiscount = 0;
+		switch(order.getCustomerDetails().getCustomerType()) {
+			case  EMPLOYEE :
+				if(amountWithoutGrocery!=0) {
+					percentDiscount = (amountWithoutGrocery * Constants.EMP_DISCOUNT)/100;
+				}
+				break;
+			case  AFFILIATE :
+				if(amountWithoutGrocery!=0) {
+					percentDiscount = (amountWithoutGrocery * Constants.AFL_DISCOUNT)/100;
+				}
+				break;
+			case  CUSTOMER :
+				if(amountWithoutGrocery!=0) {
+					long diffInMillies = Math.abs(order.getCurrentDate().getTime() - order.getCustomerDetails().getCreationDate().getTime());
+				    long diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+				    
+					percentDiscount = diff >= 365*2 ? (amountWithoutGrocery * Constants.CUST_TWO_YRS)/100 : 0;
+				}
+				break;
+			default:
+				percentDiscount = 0;
+		}
+		
+		int extraDiscount = totalAmount >= 100 ? (totalAmount/100)*5 : 0;
+		int payableAmount = totalAmount - (percentDiscount + extraDiscount);
+		return payableAmount;
+	}
+
+}
