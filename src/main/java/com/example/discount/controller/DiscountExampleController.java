45dcc727d1f6a1c2199cc1e40113759dff18df17
pathak001 <54776760+pathak001@users.noreply.github.com>
2019-10-01 01:21:41 +0530

    update file

--- src/main/java/com/example/discount/controller/DiscountExampleController.java
+++ src/main/java/com/example/discount/controller/DiscountExampleController.java
@@ -1,1 +1,26 @@
+package com.example.discount.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.discount.model.Order;
+import com.example.discount.service.DiscountExampleService;
+
+@RestController
+public class DiscountExampleController {
+	
+	@Autowired
+	private DiscountExampleService discountExampleService;
+	
+	@PostMapping(value = "/getPayableAmount")
+	public ResponseEntity<Integer> getPayableAmount(@RequestBody Order order) {
+		
+		int payableAmount = discountExampleService.calculateDiscount(order);
+		
+		return ResponseEntity.ok(payableAmount);		
+	}
+
+}
