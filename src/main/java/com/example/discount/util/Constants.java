96fc3b4c9d3d8b958048088deab47800a8ff3c01
pathak001 <54776760+pathak001@users.noreply.github.com>
2019-10-01 01:19:35 +0530

    update class

--- src/main/java/com/example/discount/util/Constants.java
+++ src/main/java/com/example/discount/util/Constants.java
@@ -1,1 +1,9 @@
+package com.example.discount.util;
 
+public class Constants {
+	
+	public final static int EMP_DISCOUNT = 30;
+	public final static int AFL_DISCOUNT = 10;
+	public final static int CUST_TWO_YRS = 5;
+
+}
